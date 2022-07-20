d2c47d9ee7e38da5cd91100bc077774147c1cd47
Yan Amaral <yan@pop-os.localdomain>
2022-07-20 09:29:10 -0300

    Tela de Inicio

---
+++ src/LandingPage.js
@@ -0,0 +1,11 @@
+function LandingPage () {
+    return (
+        <div className="page">
+            <img className="logo" src="assets/logo.png" />
+            <div className="title">ZapRecall</div>
+            <div className="start">Iniciar Recall!!</div>
+        </div>
+    );
+}
+
+export default LandingPage;
